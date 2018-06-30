0fd5971fba82e654cb5a8d5b9555b01a76dbcc52
Ivan S <ivanms1@gmail.com>
2018-06-30 18:03:23 +0700

    tests partially added

--- schema/User.js
+++ schema/User.js
@@ -1,18 +1,37 @@
-const sequelize = require('../db/postgres');
 const Sequelize = require('sequelize');
 const bcrypt = require('bcryptjs');
+const validate = require('validator')
+// Set the database according the to the ENV
+const sequelize = process.env.NODE_ENV === 'test' ?
+                require('../test/test-db') :
+                require('../db/postgres');
 
 const User = sequelize.define('user', {
     email: {
         type: Sequelize.STRING,
-        unique: true
+        unique: true,
+        validate: {
+            isEmail: {
+                msg: 'Email not valid'
+            },
+            
+        },
+        allowNull: false,
     },
-    password: Sequelize.STRING
+    password: {
+        type: Sequelize.STRING,
+        validate: {
+            len: {
+                args: [6,40],
+                msg: "Password is too short or too long"
+            }
+        }
+    }
  });
 
  User.beforeCreate((user, options) => {
-    const salt = bcrypt.genSalt(10);
-    user.password = user.password && user.password != "" ? bcrypt.hashSync(user.password, 10) : "";
+    const salt = bcrypt.genSaltSync(10);
+    user.password = user.password && user.password != "" ? bcrypt.hashSync(user.password, salt) : "";
  });
 
  module.exports = User;
---
+++ test/user.js
@@ -0,0 +1,142 @@
+const User = require('../schema/User');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+let user = {
+    email: 'test@test.com',
+    password: '123456'
+}
+
+let oldUser = {
+    email: 'test2@test.com',
+    password: '123456'
+}
+
+let wrongEmail = {
+    email: 'test3@test.com',
+    password: '123456'
+}
+
+let wrongPassword = {
+    email: 'test2@test.com',
+    password: '1234567'
+}
+
+let badEmail = {
+    email: 'test',
+    password: '123456'
+}
+
+let badPassword = {
+    email: 'test@test.com',
+    password: '12345'
+}
+
+describe('User', () => {
+    beforeEach((done) => {
+        User.destroy({
+            where: {},
+            truncate: true
+        })
+        .then(() => {
+            User.create(oldUser)
+            .then(() => done());
+        });
+    });
+
+    describe('/signup user', () => {
+        it('should sign up the user', (done) => {
+            chai.request(server)
+            .post('/user/signup')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.a('object');
+                done();
+            });
+        });
+
+        it('should not sign up if email already exists', (done) => {
+            chai.request(server)
+            .post('/user/signup')
+            .send(oldUser)
+            .end((err, res) => {
+               res.should.have.status(400);
+               done(); 
+            });
+        });
+
+        it('should accept only a valid email', (done) => {
+            chai.request(server)
+            .post('/user/signup')
+            .send(badEmail)
+            .end((err, res) => {
+                if(err) done(err);
+                res.should.have.status(400);
+                done();
+            });
+        });
+
+        it('should only accept a valid password', (done) => {
+            chai.request(server)
+            .post('/user/signup')
+            .send(badPassword)
+            .end((err, res) => {
+                if(err) done(err);
+                res.should.have.status(400);
+                done();
+            });
+        });
+    });
+
+    describe('/login user', () => {
+        it('should login the user', (done) => {
+            chai.request(server)
+            .post('/user/login')
+            .send(oldUser)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('session');
+
+                let token = res.body.session;
+                
+                chai.request(server)
+                .get('/user/current')
+                .set('Authorization', 'Bearer ' + token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+            });
+        });
+
+        it('should not login if user does not exists', (done) => {
+            chai.request(server)
+            .post('/user/login')
+            .send(wrongEmail)
+            .end((err, res) => {
+                res.should.have.status(404);
+                done()
+            });
+        });
+
+        it('should not login the user if the password is incorrect', (done) => {
+            chai.request(server)
+            .post('/user/login')
+            .send(wrongPassword)
+            .end((err, res) => {
+                res.should.have.status(400);
+                done()
+            });
+        });
+    });
+});
+
+
+
